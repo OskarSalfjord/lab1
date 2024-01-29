f3e0ac66c48e208adc47ebea1a9d9bd0209a0bc8
oskar <oskarsal@net.chalmers.se>
2024-01-29 16:06:24 +0100

    1

--- Cartransport.java
+++ Cartransport.java
@@ -1,16 +1,19 @@
 import java.awt.*;
 import java.util.Stack;
 
-public abstract class Cartransport extends Truck {
+public abstract class Cartransporter3000 extends Truck {
     private enum Ramp {
         RAISED, LOWERED
     }
 
-    private Stack<Car> carRamp = new Stack<>();
+    private Stack<Car> loadedCars = new Stack<>();
     private Ramp currentRampState;
 
-    public Cartransport() {
+    private int maximumLoad;
+
+    public Cartransporter3000() {
         super(2, Color.darkGray, 500, "Cartransporter3000");
+        this.maximumLoad = 4;
         this.currentRampState = Ramp.RAISED;
     }
 
@@ -23,27 +26,29 @@ public abstract class Cartransport extends Truck {
     }
 
     protected void lowerRamp() {
-        this.currentRampState = Ramp.LOWERED;
+        if(currentSpeed == 0) {
+            this.currentRampState = Ramp.LOWERED;
+        }
+        else{
+            throw new IllegalCallerException("The car transport is moving, ramp cannot be lowered");
+        }
     }
-}
+
     protected void loadCar(Car carToLoad) {
         if (carToLoad.getLoadable()) {
-            carRamp.add(carToLoad);
+            if (getRamp() == Ramp.LOWERED) {
+                if (loadedCars.size() < maximumLoad) {
+                    loadedCars.add(carToLoad);
+
+                } else {
+                    throw new IllegalArgumentException("The car transport is full");
+                }
+            } else {
+                throw new IllegalArgumentException("The ramp is raised, car can not be loaded");
+            }
         }
         else {
-            throw new IllegalArgumentException("This car can not be loaded onto the ramp");
-        }
-
-    }
-
-    @Override
-    protected void startEngine(){
-        if (getCanBeLoaded()) {
-            throw new IllegalCallerException("The ramp is currently down, Close it before starting the engine");
-        }
-        else {
-            currentSpeed = 0.1;
-        }
+                throw new IllegalArgumentException("The car is not loadable");
     }
 
     @Override
